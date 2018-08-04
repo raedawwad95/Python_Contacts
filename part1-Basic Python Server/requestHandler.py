474781bf4b9d8c8c1ed3a6105bed201db5c83360
raedawwad95 <awwad12121@gmail.com>
2018-08-04 13:52:02 +0300

    part1

--- part1-Basic Python Server/requestHandler.py
+++ part1-Basic Python Server/requestHandler.py
@@ -7,12 +7,12 @@ class myHandler(BaseHTTPRequestHandler):
 	#This will handle GET requests
 	def do_GET(self):
 		# Add the outgoing status here
-		
+		self.send_response(200)
 		# You will need to change this if you are sending something
   		# other than plain text, like JSON or HTML.
 		self.send_header('Content-type','text/html')
 		self.end_headers()
-
-		# Send the "Hello World" message here 
 		
+		# Send the "Hello World" message here 
+		self.wfile.write("Hello World")
 		return
--- part1-Basic Python Server/server.py
+++ part1-Basic Python Server/server.py
@@ -1,6 +1,6 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 #Start by importing myHandler Class from requestHandler.py file
-
+from requestHandler import myHandler
 #Every server needs to listen on a port with a unique number. The
 #standard port for HTTP servers is port 8000,
 PORT_NUMBER = 8080
@@ -8,7 +8,7 @@ PORT_NUMBER = 8080
 try:
 	#This statement creates a web server and define the handler to manage the
 	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), 'Fill_Me_In')
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
 	print 'Started httpserver on port ' , PORT_NUMBER
 	
 	#Wait forever for incoming htto requests
